8b93744806ae431c1ec36da21aa2833f478b2d62
donghquinn <ehdgus1524@gmail.com>
2023-11-10 13:20:53 +0900

    wip

---
+++ python_modules/cnn/model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch
+
+
+class CnnModel(nn.Module):
+    def __init__(self):
+        super(CnnModel, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, kernel_size=5, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        out, _ = self.layer1(x)
+
+        out2, _ = self.layer2(out)
+
+        out3, _ = self.fc(out2)
+
+        return out3
